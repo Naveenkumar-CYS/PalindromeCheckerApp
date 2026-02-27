c0962d4345c0c4c3cbf547cd798bb3a9f8f466fd
Naveenkumar-CYS <naveenkumar2007cs@gmail.com>
2026-02-27 14:57:41 +0530

    UC2: User input palindrome check implemented

--- Palindrome-checker/src/PalindromeCheckerApp.java
+++ Palindrome-checker/src/PalindromeCheckerApp.java
@@ -9,28 +9,22 @@ public class PalindromeCheckerApp {
         System.out.println("   Version: 1.0");
         System.out.println("=====================================");
 
-        // Create Scanner object
         Scanner scanner = new Scanner(System.in);
 
-        // Ask user input
-        System.out.print("Enter a word to check: ");
+        System.out.print("Enter a word: ");
         String word = scanner.nextLine();
 
-        // Reverse string
         String reversed = "";
 
         for (int i = word.length() - 1; i >= 0; i--) {
             reversed += word.charAt(i);
         }
 
-        // Palindrome check
         if (word.equals(reversed)) {
             System.out.println(word + " is a Palindrome.");
         } else {
             System.out.println(word + " is NOT a Palindrome.");
         }
-
-        System.out.println("Program Completed.");
 
         scanner.close();
     }
