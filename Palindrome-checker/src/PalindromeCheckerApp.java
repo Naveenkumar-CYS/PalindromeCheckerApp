916ae85f84056d072a214cfff1e76a1b2f039814
Naveenkumar-CYS <naveenkumar2007cs@gmail.com>
2026-02-27 14:47:04 +0530

    Save current changes before switching branch

--- Palindrome-checker/src/PalindromeCheckerApp.java
+++ Palindrome-checker/src/PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -6,6 +8,30 @@ public class PalindromeCheckerApp {
         System.out.println("   WELCOME TO PALINDROME CHECKER");
         System.out.println("   Version: 1.0");
         System.out.println("=====================================");
-        System.out.println("Application Started Successfully.");
+
+        // Create Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask user input
+        System.out.print("Enter a word to check: ");
+        String word = scanner.nextLine();
+
+        // Reverse string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        // Palindrome check
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
+
+        System.out.println("Program Completed.");
+
+        scanner.close();
     }
 }
