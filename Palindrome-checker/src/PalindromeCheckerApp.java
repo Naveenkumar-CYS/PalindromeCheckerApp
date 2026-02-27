e64c411061445adc55b6ab167ec332fc3665cd25
Naveenkumar-CYS <naveenkumar2007cs@gmail.com>
2026-02-27 14:09:48 +0530

    Base application setup

---
+++ Palindrome-checker/src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("   WELCOME TO PALINDROME CHECKER");
+        System.out.println("   Version: 1.0");
+        System.out.println("=====================================");
+        System.out.println("Application Started Successfully.");
+    }
+}
